c9632891cfbe0966a7c13017a4f48cb3018f58a3
rykktiadloans <datokar1705@gmail.com>
2025-08-26 10:49:54 +0200

    Hold on

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "FastAPI flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default =
+      pkgs.mkShell 
+        {
+          buildInputs = [
+            pkgs.python312
+          ];
+
+          shellHook = ''
+            echo "FastAPI flake started"
+          '';
+
+        };
+
+    #packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    #packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
