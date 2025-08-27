926472cfe470f2a017c00b8b4fdc6bd1002cec7f
rykktiadloans <datokar1705@gmail.com>
2025-08-27 19:00:23 +0200

    moved the input field to the separate component and made it more responsive

---
+++ frontend/src/components/FormField.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  name: string;
+  label: string;
+}
+
+function FormField({ label, name }: Props) {
+  return (
+    <div className="flex flex-col gap-0.5">
+      <label htmlFor={name}>{label}</label>
+      <input
+        type="text"
+        name={name}
+        id={name}
+        className="w-full transition-colors border-1 border-gray-800
+              hover:border-emerald-400 rounded-md p-1"
+      />
+    </div>
+  );
+}
+
+export default FormField;
