27e23be21a014591268deabbc2a2126cd82e9f39
rykktiadloans <datokar1705@gmail.com>
2025-08-27 18:41:07 +0200

    forgot to also add all that

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Chat from "./views/Chat";
+import Register from "./views/Register";
+import Login from "./views/Login";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Chat />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
